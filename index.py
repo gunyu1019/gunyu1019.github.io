63082b9b4bcda2c482495f053fdf7f67ebf4d18c
gunyu1019 <gunyu1019@gmail.com>
2026-02-07 23:19:25 +0900

    [Feat] Add project description more modal.

--- index.py
+++ index.py
@@ -9,6 +9,8 @@ import virtual_business_card
 # noinspection PyUnresolvedReferences
 import project
 
+import json
+
 if not is_server_side:
     import js
 
@@ -17,6 +19,14 @@ application = Application()
 
 @application.page()
 class MainPage(Page):
+    def initial(self):
+        f = open("project.json", mode="r", encoding='utf-8')
+        project_info = json.load(f)
+        f.close()
+        return {
+            "data": project_info
+        }
+
     def populate(self):
         with t.div(classes=["swiper", "swiper-container"]):
             with t.div(classes=["swiper-wrapper"]):
@@ -24,6 +34,20 @@ class MainPage(Page):
                 t.project(data_anchor="project", classes=["swiper-slide"])
                 t.virtual_business_card(data_anchor="virtual_business_card", classes=["swiper-slide"])
             t.div(classes=["swiper-pagination"])
+
+        # Modal for long project description
+        with t.div(classes=["modal-group"]):
+            for project_data in self.state["data"]:
+                project_unique_id = project_data["title"].replace(" ", "-") + "-modal"
+                with t.div(classes=["modal fade"], tabindex="-1", id=project_unique_id,
+                           aria_labelledby=f"{project_unique_id}_label", aria_hidden="true"):
+                    with t.div(classes=["modal-dialog modal-dialog-centered modal-lg"]):
+                        with t.div(classes=["modal-content"]):
+                            with t.div(classes=["modal-header"]):
+                                t.h5(project_data["title"], classes=["modal-title"], id=f"{project_unique_id}_label")
+                                t.button(classes=["btn-close"], data_bs_dismiss="modal", aria_label="Close")
+                            with t.div(classes=["modal-body"]):
+                                t.p("\n".join(project_data["description"]))
         return
 
     def on_ready(self):
